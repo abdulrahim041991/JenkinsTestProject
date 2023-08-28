3eeff3393a9dfdb6bd8c2b20ffa70c432dea9686
abdulrahim <abdulrahim.shaikh041991@gmail.com>
2023-08-28 22:31:29 +0530

    Jenkins Test Project

---
+++ src/main/java/com/example/JenkinsTest/MyController.java
@@ -0,0 +1,14 @@
+package com.example.JenkinsTest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@RequestMapping(value = "/SayHello" , method = RequestMethod.GET)
+	public String SayHello() {
+		return "Hello Jenkins";
+	}
+}
