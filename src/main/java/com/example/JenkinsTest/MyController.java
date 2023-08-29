6a0a6d9dfb06238010102c5235e94e82568f3be9
abdulrahim <abdulrahim.shaikh041991@gmail.com>
2023-08-30 00:40:42 +0530

    update 23

--- src/main/java/com/example/JenkinsTest/MyController.java
+++ src/main/java/com/example/JenkinsTest/MyController.java
@@ -9,6 +9,6 @@ public class MyController {
 
 	@RequestMapping(value = "/SayHello" , method = RequestMethod.GET)
 	public String SayHello() {
-		return "Hello Jenkins 2";
+		return "Hello Jenkins 3";
 	}
 }
